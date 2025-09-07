b88ba9e2233bdfc425c8e151e30d0f3b8446d5ae
EashahShujat13 <eashahofficework@gmail.com>
2025-09-07 23:39:13 +0500

    again

--- index.mjs
+++ index.mjs
@@ -12,7 +12,7 @@ db.connection.once('open', () => console.log("connected to db")).on("error", (er
 app.use(cors())
 app.use(express.json())
 app.get("/", (req, res) => {
-  res.send("🚀 API is running on Vercel");
+  res.send("🚀 API is running on Vercel with mistake changes done");
   
 });
 app.use("/",router)
